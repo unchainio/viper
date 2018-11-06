9048928eb7b97b2dc7ca1c16501e6b8a39c80d2c
Emil Nikolov <emil.e.nikolov@gmail.com>
2018-11-06 23:35:41 +0100

    fixed tests

--- viper_test.go
+++ viper_test.go
@@ -587,7 +587,7 @@ func TestBindPFlagsStringSlice(t *testing.T) {
 		Expected []string
 		Value    string
 	}{
-		{[]string{}, ""},
+		{nil, ""},
 		{[]string{"jeden"}, "jeden"},
 		{[]string{"dwa", "trzy"}, "dwa,trzy"},
 		{[]string{"cztery", "piec , szesc"}, "cztery,\"piec , szesc\""}} {
